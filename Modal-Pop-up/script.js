5c4ff9a92051a9810ca9ab092e03399b6f6cbff0
R.Pavan Karthik <Rpavankarthik18@gmail.com>
2023-11-11 15:10:32 -0600

    Added Code Comments

--- Modal-Pop-up/script.js
+++ Modal-Pop-up/script.js
@@ -1,29 +1,35 @@
-'use strict';
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
+"use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
 
+//function to open the modal
 const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
+//function to close the modal
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
+//Providing click events to all the available buttons
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', openModal);
+  btnsOpenModal[i].addEventListener("click", openModal);
 }
 
-btnCloseModal.addEventListener('click', closeModal);
+//Click Event Listener to close the modal when clicked on close button
+btnCloseModal.addEventListener("click", closeModal);
 
-overlay.addEventListener('click', closeModal);
+//Click Event Listener to close the modal when clicked on the overlay area
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key == 'Escape' && !modal.classList.contains('hidden')) {
+//Click Event Listener to close the modal when Escape key is pressed
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
